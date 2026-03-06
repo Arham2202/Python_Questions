d2f631474f35c119cf8b71a83b9398a4e41275c3
Arham Khan <mdarhamkhan22@gmail.com>
2026-03-06 11:56:47 +0530

    Q21 Python program that determines whether a given number (accepted from the user) is even or odd, and prints an appropriate message to the user.

---
+++ Q21.py
@@ -0,0 +1,9 @@
+#Q21 Python program that determines whether a given number (accepted from the user) is even or odd, and prints an appropriate message to the user.
+num = int(input("Enter the number: "))
+
+mod = num % 2
+
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
